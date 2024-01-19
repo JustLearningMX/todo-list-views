106cdf3487e4ec10b9ece97ed632fbe21f9ddeff
Hiram Chavez <virtual.liga@gmail.com>
2024-01-19 11:12:41 -0600

    Adding primeng, primeicons, primeflex. Creating primeNg Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PrimeNGConfig} from "primeng/api";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo-list-views';
+export class AppComponent implements OnInit {
+
+  constructor(private primeConfig: PrimeNGConfig) {}
+
+  title = 'Mis Tareas';
+
+  ngOnInit() {
+    this.primeConfig.ripple = true;
+  }
+
 }
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import {MenuModule} from "primeng/menu";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MenuModule
+  ]
+})
+export class PrimeNgModule { }
