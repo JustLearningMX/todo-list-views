7400521fbf7795d8367318c860f243a5785a59ee
Hiram Chavez <virtual.liga@gmail.com>
2024-01-20 14:17:40 -0600

    Configure PrimeNG Module

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import {MenuModule} from "primeng/menu";
+
+import { MenuModule } from "primeng/menu";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
   declarations: [],
-  imports: [
-    MenuModule
-  ]
+  exports: [
+    MenuModule,
+    ButtonModule
+  ],
 })
 export class PrimeNgModule { }
